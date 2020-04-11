5b3fc32a538e85be1f5e97f05cd08c064c7398da
fordluttrell <54590597+fordluttrell@users.noreply.github.com>
2020-04-10 19:21:16 -0500

    Update Balance.java

--- Balance.java
+++ Balance.java
@@ -1,10 +1,19 @@
 package com.example.moneymanagment;
 
-public class Balance {
+public class Balance
+{
+   
     public double bal;
-    public String toSQL() {
-        StringBuilder sb = new StringBuilder("(");
-        sb.append(bal).append(",");
-        return sb.toString();
+    
+    public String toSQL() 
+    
+    {
+    
+        StringBuilder sb = new StringBuilder ("(");
+        
+        sb.append (bal).append (",");
+        
+        return sb.toString ();
     }
+    
 }
