3ec85c797dcec20842cacaf8141f1245022eb99a
fordluttrell <54590597+fordluttrell@users.noreply.github.com>
2020-04-10 19:11:08 -0500

    Create Balance.java

---
+++ Balance.java
@@ -0,0 +1,10 @@
+package com.example.moneymanagment;
+
+public class Balance {
+    public double bal;
+    public String toSQL() {
+        StringBuilder sb = new StringBuilder("(");
+        sb.append(bal).append(",");
+        return sb.toString();
+    }
+}
